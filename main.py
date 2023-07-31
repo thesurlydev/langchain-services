28f484f9251624f82b50a017ecec32dff7942fa3
Shane Witbeck <shane@digitalsanctum.com>
2023-07-31 11:29:34 -0700

    feat: add company address and focus endpoints

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
+import json
 import os
+from datetime import datetime, timedelta
+from typing import Type
 
+import yfinance as yf
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from langchain import LLMMathChain, SerpAPIWrapper
+from langchain.agents import AgentType
+from langchain.agents import initialize_agent, Tool
 from langchain.chat_models import ChatOpenAI
-from langchain.schema import (
-    HumanMessage
-)
-from pydantic import BaseModel
+from langchain.schema import HumanMessage
+from langchain.tools import BaseTool
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
@@ -32,16 +38,174 @@ def read_root():
     return {"Hello": "world"}
 
 
-@app.post('/')
-def answer_query(question: Question):
-    try:
-        query = question.query
-        openai_api_key = os.environ.get("OPENAI_API_KEY")
+class CurrentStockPriceInput(BaseModel):
+    """Inputs for get_current_stock_price"""
+    ticker: str = Field(description="Ticker symbol of the stock")
 
-        chat = ChatOpenAI(temperature=0, openai_api_key=openai_api_key)
-        message = [HumanMessage(content=query)]
-        output = chat(message)
-        answer = Answer(query=query, answer=output.content)
-        return answer
-    except:
-        return {"message": "Error!"}
+
+def get_current_stock_price(ticker):
+    """Method to get current stock price"""
+
+    ticker_data = yf.Ticker(ticker)
+    recent = ticker_data.history(period='1d')
+    return {
+        'price': recent.iloc[0]['Close'],
+        'currency': ticker_data.info['currency']
+    }
+
+
+def get_stock_performance(ticker, days):
+    """Method to get stock price change in percentage"""
+
+    past_date = datetime.today() - timedelta(days=days)
+    ticker_data = yf.Ticker(ticker)
+    history = ticker_data.history(start=past_date)
+    old_price = history.iloc[0]['Close']
+    current_price = history.iloc[-1]['Close']
+    return {
+        'percent_change': ((current_price - old_price) / old_price) * 100
+    }
+
+
+class CurrentStockPriceTool(BaseTool):
+    name = "get_current_stock_price"
+    description = """
+        Useful when you want to get current stock price.
+        You should enter the stock ticker symbol recognized by the yahoo finance
+        """
+    args_schema: Type[BaseModel] = CurrentStockPriceInput
+
+    def _run(self, ticker: str):
+        price_response = get_current_stock_price(ticker)
+        return price_response
+
+    def _arun(self, ticker: str):
+        raise NotImplementedError("get_current_stock_price does not support async")
+
+
+class StockPercentChangeInput(BaseModel):
+    """Inputs for get_stock_performance"""
+    ticker: str = Field(description="Ticker symbol of the stock")
+    days: int = Field(description='Timedelta days to get past date from current date')
+
+
+class StockPerformanceTool(BaseTool):
+    name = "get_stock_performance"
+    description = """
+        Useful when you want to check performance of the stock.
+        You should enter the stock ticker symbol recognized by the yahoo finance.
+        You should enter days as number of days from today from which performance needs to be check.
+        output will be the change in the stock price represented as a percentage.
+        """
+    args_schema: Type[BaseModel] = StockPercentChangeInput
+
+    def _run(self, ticker: str, days: int):
+        response = get_stock_performance(ticker, days)
+        return response
+
+    def _arun(self, ticker: str):
+        raise NotImplementedError("get_stock_performance does not support async")
+
+
+@app.post('/agent')
+def agent(question: Question):
+    print(question)
+    llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo-0613")
+    search = SerpAPIWrapper()
+    llm_math_chain = LLMMathChain.from_llm(llm=llm, verbose=True)
+
+    tools = [
+        CurrentStockPriceTool(),
+        StockPerformanceTool(),
+        Tool(
+            name="Search",
+            func=search.run,
+            description="useful for when you need to answer questions about current events. You should ask targeted questions"
+        ),
+        Tool(
+            name="Calculator",
+            func=llm_math_chain.run,
+            description="useful for when you need to answer questions about math"
+        )
+    ]
+    agent = initialize_agent(tools, llm, agent=AgentType.OPENAI_FUNCTIONS, verbose=True)
+    query = question.query
+    output = agent.run(query)
+    print(output)
+    answer = Answer(query=query, answer=output)
+    return answer
+
+
+@app.post('/questions')
+def answer_query(question: Question):
+    query = question.query
+    openai_api_key = os.environ.get("OPENAI_API_KEY")
+    chat = ChatOpenAI(temperature=0, openai_api_key=openai_api_key)
+    message = [HumanMessage(content=query)]
+    output = chat(message)
+    answer = Answer(query=query, answer=output.content)
+    return answer
+
+@app.get('/companies/{name}/logo')
+def company_info(name: str):
+    prompt = """
+    Given the name of a company return a url for a high resolution version of the company logo. The url returned must return an actual image. The logo should be in png or svg format.
+    Return only the url for the company logo and the dimensions of the image in json format with snake casing. nothing else. The name of the company is {0}
+    """
+    query = prompt.format(name)
+    openai_api_key = os.environ.get("OPENAI_API_KEY")
+
+    chat = ChatOpenAI(temperature=0, openai_api_key=openai_api_key)
+    message = [HumanMessage(content=query)]
+    output = chat(message)
+    out = output.content
+    return json.loads(out)
+
+@app.get('/companies/{name}/focus')
+def company_info(name: str):
+    prompt = """
+    I am a software engineer and writing a cover letter about why I want to work at a technology company.  
+    Given the name of the company, I want you to provide no more than two paragraphs specific to the company's focus 
+    that resonates with my professional ethos and makes me excited about the potential of working at the company. 
+    The paragraphs should be in the first person and addressed to the company.
+    Return only the company name and focus paragraphs in json format with the field names being company_name and focus with snake casing. nothing else.
+    The name of the company is {0}.
+    """
+    query = prompt.format(name)
+    openai_api_key = os.environ.get("OPENAI_API_KEY")
+
+    chat = ChatOpenAI(temperature=0.5, openai_api_key=openai_api_key)
+    message = [HumanMessage(content=query)]
+    output = chat(message)
+    out = output.content
+    return json.loads(out)
+@app.get('/companies/{name}/address')
+def company_info(name: str):
+    prompt = """
+    Given the name of a company return the company name, website, and full mailing address. Return the address with separate fields for street, city, state or province, postal code and country.
+    Return only the company name, website, and full mailing address in json format with snake casing. nothing else. The name of the company is {0}
+    """
+    query = prompt.format(name)
+    openai_api_key = os.environ.get("OPENAI_API_KEY")
+
+    chat = ChatOpenAI(temperature=0, openai_api_key=openai_api_key)
+    message = [HumanMessage(content=query)]
+    output = chat(message)
+    out = output.content
+    return json.loads(out)
+@app.get('/companies/{name}')
+def company_info(name: str):
+    prompt = """
+    Given the name of a company return the company name, ratings from indeed.com and glassdoor.com, top revenue producing products, social media urls, estimated annual revenue in millions of dollars, associated companies and relationship and website, company description, sector, industry, the year it started, ticker symbol, headquarters address, coordinates of headquarters location, exchange,
+    number of employees, the url on linkedin.com, glassdoor.com and indeed.com and teamblind.com, website, careers url, and top five competitors including website.
+    Return only the company name, ratings from indeed.com and glassdoor.com, top revenue producing products, social media urls, estimated annual revenue, associated companies and relationship and website, company description, sector, industry, the year it started, ticker symbol, exchange, headquarters address, coordinates of headquarters location, number of employees, urls,
+    website, careers url, and top five competitors including website in json format with snake casing. nothing else. the name of the company is {0}
+    """
+    query = prompt.format(name)
+    openai_api_key = os.environ.get("OPENAI_API_KEY")
+
+    chat = ChatOpenAI(temperature=0, openai_api_key=openai_api_key)
+    message = [HumanMessage(content=query)]
+    output = chat(message)
+    out = output.content
+    return json.loads(out)
--- play1.py
+++ play1.py
@@ -11,5 +11,3 @@ chat = ChatOpenAI(temperature=0, openai_api_key=openai_api_key)
 message = [HumanMessage(content="What is the name of the most populous state in the USA?")]
 out = chat(message)
 print(out.content)
-
-
